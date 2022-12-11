01d7a1632b7a032368a1f080798fc41d6d571f9d
KKKA404 <1738191864@qq.com>
2022-12-11 18:53:26 +0800

    add daemon

---
+++ Server/src/Daemon.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupService {
+    //add and test by mxy
+    // 定义心跳消息
+    public  String HEARTBEAT_MESSAGE = "I'm still alive";
+    // 定义心跳频率（每隔10秒发送一次心跳）
+    public  int HEARTBEAT_INTERVAL = 10;
+    // 定义组成员列表
+    public  List<String> memberList = new ArrayList<>();
+
+
+}
