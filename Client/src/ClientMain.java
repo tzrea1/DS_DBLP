0d4d4f918a8c26e593be6ea6314c38e4ea2e3568
root <3062941557@qq.com>
2022-12-15 02:35:33 +0800

    Support multiple thread send

--- Client/src/ClientMain.java
+++ Client/src/ClientMain.java
@@ -36,7 +36,7 @@ public class ClientMain {
                 System.out.println("输入不合法，请重新输入");
             }
         }
-        while(true){
+        while(true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("请输入作者姓名:");
             //输入姓名，测试可用：Ion Stoica
@@ -54,8 +54,8 @@ public class ClientMain {
             System.out.println("正在查询.....");
             //创建计时
             long startTime = System.currentTimeMillis();
-            int numWithoutYear=-1;
-//            // 调用Query.queryByName进行查询
+            // 调用Query.queryByName进行查询
+//            int numWithoutYear=-1;
 //            // 访问每一台虚拟机进行查询
 //            for(int i=0;i<3;i++){
 //                for(int j=0;j<2;j++) {
@@ -63,23 +63,72 @@ public class ClientMain {
 //                }
 //            }
             // 调用Query.queryByNameAndYear进行查询
-            int numWithYear=-1;
-            // 访问每一台虚拟机进行查询
-            for(int i=0;i<3;i++){
-                for(int j=0;j<2;j++) {
-                    numWithYear += AccessServer.sendQuery(name, beginYear, endYear,i,j);
+            final int[] numWithYear = {-2, -2, -2, -2, -2, -2};
+            int numAll = 0;
+//            // 访问每一台虚拟机进行查询
+//            for (int i = 0; i < 3; i++) {
+//                for (int j = 0; j < 2; j++) {
+//                    numAll += AccessServer.sendQuery(name, beginYear, endYear, i, j);
+//                }
+//            }
+            Thread thread1 = new Thread(new Runnable() {
+                public void run() {
+                    numWithYear[0] = AccessServer.sendQuery(name, beginYear, endYear,0,0);
                 }
+            });
+            Thread thread2 = new Thread(new Runnable() {
+                public void run() {
+                    numWithYear[1] = AccessServer.sendQuery(name, beginYear, endYear,0,1);
+                }
+            });
+            Thread thread3 = new Thread(new Runnable() {
+                public void run() {
+                    numWithYear[2] = AccessServer.sendQuery(name, beginYear, endYear,1,0);
+                }
+            });
+            Thread thread4 = new Thread(new Runnable() {
+                public void run() {
+                    numWithYear[3] = AccessServer.sendQuery(name, beginYear, endYear,1,1);
+                }
+            });
+            Thread thread5 = new Thread(new Runnable() {
+                public void run() {
+                    numWithYear[4] = AccessServer.sendQuery(name, beginYear, endYear,2,0);
+                }
+            });
+            Thread thread6 = new Thread(new Runnable() {
+                public void run() {
+                    numWithYear[5] = AccessServer.sendQuery(name, beginYear, endYear,2,1);
+                }
+            });
+            // 启动Send线程
+            thread1.start();
+            thread2.start();
+            thread3.start();
+            thread4.start();
+            thread5.start();
+            thread6.start();
+
+            // 等待线程进行
+            try {
+                thread1.join();
+                thread2.join();
+                thread3.join();
+                thread4.join();
+                thread5.join();
+                thread6.join();
+            } catch (Exception e) {
+                System.out.println("thread error");
             }
-            if(numWithoutYear==-1||numWithYear==-1)
-                System.out.println("连接出错！");
-            else {
-                //输出用时
-                long endTime = System.currentTimeMillis();
-                System.out.println("查询成功! 用时："+ (double) (endTime - startTime) / 1000 + "s");
-                //输出查询结果
-                System.out.println("没有年份限制时，"+name+"的DBLP发表论文总数为：" + numWithoutYear);
-                System.out.println("有年份限制时，"+name+"的DBLP发表论文总数为：" + numWithYear);
-            }
+
+            // 总的论文频次数
+            numAll=numWithYear[0]+numWithYear[1]+numWithYear[2]+numWithYear[3]+numWithYear[4]+numWithYear[5];
+            //输出用时
+            long endTime = System.currentTimeMillis();
+            System.out.println("查询成功! 用时：" + (double) (endTime - startTime) / 1000 + "s");
+            //输出查询结果
+            //System.out.println("没有年份限制时，"+name+"的DBLP发表论文总数为：" + numWithoutYear);
+            System.out.println("有年份限制时，" + name + "的DBLP发表论文总数为：" + numAll);
         }
     }
 }
